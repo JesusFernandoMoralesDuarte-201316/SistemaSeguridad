192b3f7043b936131f67a071748cc10ec9aaca22
JesusFernandoMoralesDuarte-201316 <al201316@alumnos.uacj.mx>
2024-09-15 23:06:11 -0600

    CSRF

--- SistemaInventarios/settings.py
+++ SistemaInventarios/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-44x!5l9y457v1v+#l-zzz2um=18sv)0z!o#qe_1o3@(@f#z0rp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ['localhost','https://sistemaseguridad-production.up.railway.app']
 ALLOWED_HOSTS = ['*']
@@ -79,10 +83,7 @@ WSGI_APPLICATION = 'SistemaInventarios.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 
